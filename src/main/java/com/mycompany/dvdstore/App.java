ca875c3d0436ea57a885032704766da3680e4b3a
Alice Spagnesi <a.spagnesi@patriarche.fr>
2024-10-28 22:24:28 +0100

    feat: DVDStore ex13

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,8 +3,11 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller","com.mycompany.dvdstore.service","com.mycompany.dvdstore.repository.file"})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
